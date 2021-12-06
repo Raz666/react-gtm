882711080fc3019b2f084746f54c040d38327bcb
Gabriela Menkiewicz <gabriela.menkiewicz@britishcouncil.org>
2021-12-06 12:32:36 +0100

    bump packages, remove trailing code, update tests

--- src/__tests__/Snippets.spec.js
+++ src/__tests__/Snippets.spec.js
@@ -1,40 +1,51 @@
-import Snippets from '../Snippets'
+import Snippets from "../Snippets";
 
-let args
-let snippets
+let args;
+let snippets;
 
-describe('Snippets', () => {
+describe("Snippets", () => {
   beforeEach(() => {
-    args = { id: 'GTM-abc123', dataLayerName: 'dataLayer', events: {} }
-    snippets = Snippets.tags(args)
-  })
-  
-  it('should use the `id` for the iframe', () => {
-    expect(snippets.iframe).toContain(`id=${args.id}`, 1)
-  })
-  
-  it('should use the `gtm_auth` and `gtm_preview` for the iframe', () => {
+    args = { id: "GTM-abc123", dataLayerName: "dataLayer", events: {} };
+    snippets = Snippets.tags(args);
+  });
+
+  it("should use the `id` for the iframe", () => {
+    expect(snippets.iframe).toContain(`id=${args.id}`, 1);
+  });
+
+  it("should use the `gtm_auth` and `gtm_preview` for the iframe and script", () => {
     Object.assign(args, {
-      auth: '6sBOnZx1hqPcO01xPOytLK',
-      preview: 'env-2'
-    })
-    snippets = Snippets.tags(args)
-    expect(snippets.iframe).toContain(`gtm_auth=${args.auth}`, 1)
-    expect(snippets.iframe).toContain(`gtm_preview=${args.preview}`, 1)
-  })
+      auth: "6sBOnZx1hqPcO01xPOytLK",
+      preview: "env-2",
+    });
+    snippets = Snippets.tags(args);
+    expect(snippets.iframe).toContain(`gtm_auth=${args.auth}`, 1);
+    expect(snippets.iframe).toContain(`gtm_preview=${args.preview}`, 1);
+    expect(snippets.script).toContain(`gtm_auth=${args.auth}`, 1);
+    expect(snippets.script).toContain(`gtm_preview=${args.preview}`, 1);
+  });
 
-  it('should use the `dataLayer` for the script', () => {
-    args = { dataLayer: { name: 'test' } }
-    const dataLayerName = 'dataLayer'
-    snippets = Snippets.dataLayer(args, dataLayerName)
-    console.log(snippets)
-    expect(snippets).toContain('{"name":"test"}')
-  })
+  it("should not contain `gtm_auth` nor `gtm_preview` for the iframe nor script", () => {
+    Object.assign(args);
+    snippets = Snippets.tags(args);
+    expect(snippets.iframe.includes('gtm_auth=')).toBeFalsy();
+    expect(snippets.iframe.includes('gtm_preview=')).toBeFalsy();
+    expect(snippets.script.includes('gtm_auth=')).toBeFalsy();
+    expect(snippets.script.includes('gtm_preview=')).toBeFalsy();
+  });
 
-  it('no id provided should log a warn', () => {
-      console.warn = jest.fn()
-      const noIdArgs = { dataLayerName: 'dataLayer', events: {} }
-      Snippets.tags(noIdArgs)
-      expect(console.warn).toBeCalled()
-  })
-})
+  it("should use the `dataLayer` for the script", () => {
+    args = { dataLayer: { name: "test" } };
+    const dataLayerName = "dataLayer";
+    snippets = Snippets.dataLayer(args, dataLayerName);
+    console.log(snippets);
+    expect(snippets).toContain('{"name":"test"}');
+  });
+
+  it("no id provided should log a warn", () => {
+    console.warn = jest.fn();
+    const noIdArgs = { dataLayerName: "dataLayer", events: {} };
+    Snippets.tags(noIdArgs);
+    expect(console.warn).toBeCalled();
+  });
+});
--- src/__tests__/TagManager.spec.js
+++ src/__tests__/TagManager.spec.js
@@ -1,19 +1,19 @@
-import TagManager from '../TagManager'
+import TagManager from "../TagManager";
 
-describe('TagManager', () => {
-  it('should render tagmanager', () => {
-    TagManager.initialize({gtmId: 'GTM-000000'})
-    expect(window.dataLayer).toHaveLength(1)
-  })
+describe("TagManager", () => {
+  it("should render tagmanager 1", () => {
+    TagManager.initialize({ gtmId: "GTM-000000" });
+    expect(window.dataLayer).toHaveLength(1);
+  });
 
-  it('should render datalayer', () => {
+  it("should render datalayer 2", () => {
     const gtmArgs = {
-      gtmId: 'GTM-000000',
+      gtmId: "GTM-000000",
       dataLayer: {
-        userInfo: 'userInfo'
-      }
-    }
-    TagManager.initialize(gtmArgs)
-    expect(window.dataLayer).toHaveLength(1)
-  })
-})
+        userInfo: "userInfo",
+      },
+    };
+    TagManager.initialize(gtmArgs);
+    expect(window.dataLayer).toHaveLength(1);
+  });
+});
